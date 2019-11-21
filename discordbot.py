46238c19bfc29722a8b05fb4685b11c9dd86e710
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-11-21 20:24:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1597,7 +1597,11 @@ url_embed] #ヘルプの各ページ内容
         embed.set_footer(icon_url=message.author.avatar_url, text=f"表示者｜{message.author}")
         await message.channel.send(embed = embed)
 
-        
+    if 'やられてしまった' in message.content:
+        set = randam.randrange(1,7)
+        if set == 3:
+            embed = discord.Embed(title='ゆーあーるーざーw',description=f'{message.author.mention}\n君は死んだんだよ!!\n負けたんだよ!!'
+            await message.channel.send(embed=embed)
             
 client.run(TOKEN)
 
