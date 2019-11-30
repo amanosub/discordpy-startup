fecae407fbc18a89b92a0de02e8cefffce7db6fd
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-11-30 17:02:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -758,7 +758,7 @@ url_embed] #ヘルプの各ページ内容
         userid = message.content.split('y!ban ')[1]
         embed=discord.Embed(title=f'開発者権限により、急遽対象のIDのuserをBan致します\nID:{userid}')
         await message.channel.send(embed=embed)
-        member = client.ch.guild.get_member(int(userid))
+        member = message.guild.get_member(int(userid))
         await member.ban()
         embed=discord.Embed(title='対象のIDのuserをBan完了')
         await message.channel.send(embed=embed)
