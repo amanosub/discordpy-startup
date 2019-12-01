ac4530b71f9d1a15cbc2b573c10d06e3bf59be7a
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-12-01 17:32:19 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 # discordpy-startup
 # -*- coding: utf-8 -*-
 
+
 import sys
 import discord
 import random
@@ -527,8 +528,7 @@ async def on_message(message):
     help_ch = 642578258743001088
     t_ch = client.get_channel(650537498262634497)
     await t_ch.send('::t')
-    q_check_ch = client.get_channel(650390707013550086
-    channel_id_report = 629327961132236800
+    q_check_ch = client.get_channel(65039070701355008
 
 
 #🔷test運用➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖🔷
