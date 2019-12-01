0d74a0cb9f32c1ba3b5df9a48138fc9435c63d08
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-12-01 16:56:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1045,7 +1045,7 @@ url_embed] #ヘルプの各ページ内容
                     ans_msg_embed=ans_msg.embeds[0].description
                     if ans_msg_embed.startswith('時間切れ'):
                         ans_data = re.findall('^時間切れだ。正解は「(.+)」だ。$',ans_msg_embed)
-            	    elif ans_msg_embed.startswith('残念'):
+                    elif ans_msg_embed.startswith('残念'):
                         ans_data = re.findall('^残念！正解は「(.+)」だ。$',ans_msg_embed)
                 client.t_data[ask_data]=ans_data
                 await t_ch.send('::t True->True act.2')
