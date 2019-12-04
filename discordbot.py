5dffb7e4b8ed63d1f5fb3606fd2918ea34c993ce
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-12-05 03:00:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2528,7 +2528,12 @@ async def on_message(message):
             await asyncio.sleep(1)
             await slot_em.edit(embed=discord.Embed(title=f'━━━━━━\n{A2}│{B2}│{C2}\n{A3}│{B3}│{C3}◀\n{A}│{B}│{C}',color=0x2ECC69))
             await asyncio.sleep(1)
-            await slot_em.edit(embed=discord.Embed(title=f'{A3}│{B3}│{C3}◀',description=f'結果\n{A3}{B3}{C3}',color=discord.Color.blue()))
+            if A3 == B3 and B3 == C3:
+                
+                await slot_em.edit(embed=discord.Embed(title=f'{A3}│{B3}│{C3}◀',description=f'結果\n{A3}のゾロ目だよ',color=discord.Color.blue()))
+            else:
+                
+                await slot_em.edit(embed=discord.Embed(title=f'{A3}│{B3}│{C3}◀',description=f'結果\n{A3}{B3}{C3}残念…',color=discord.Color.blue()))
 
 
 
