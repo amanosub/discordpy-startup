2cdcb1b8bab8066b65fafd2e1c5b2f4f37a5eb93
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-12-04 16:33:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -782,7 +782,7 @@ url_embed] #ヘルプの各ページ内容
             embed = discord.Embed(title = f"**Received command!!**",description = "**checking user ID** <a:loadinfo:651635984068378663>")
             test_send = await message.channel.send(embed = embed)
             await asyncio.sleep(1)
-            await test_send.embed.edit( description=f'**checking user ID** ☑️\nWould you realy need reboot {client.user.mention}?\nIf you reboot,[alrady_quiz]data will losed' )
+            message.edit(embed=discord.Embed(title = f"**Received command!!**",description=f'**checking user ID** ☑️\nWould you realy need reboot {client.user.mention}?\nIf you reboot,[alrady_quiz]data will losed' ))
             
             await test_send.add_reaction("❎")
             await test_send.add_reaction("☑️")
