3d5b5db6463784ee4cf864dda1a5e145b7498378
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-12-21 07:51:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1511,7 +1511,7 @@ async def on_message(message):
         if message.content.startswith("y!wt "):
             cityc = citycodes[message.content.split("y!wt ")[1]]
             if cityc:
-                None='None'
+               
                 resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=%s'%cityc).read()
                 resp = json.loads(resp.decode('utf-8'))
                 embed = discord.Embed(
