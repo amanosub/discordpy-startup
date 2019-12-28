afcdaa6b3b7b4eccf9f28742ee83ede5a85d19e5
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-12-28 17:00:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1512,6 +1512,7 @@ async def on_message(message):
                
                 resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=%s'%cityc).read()
                 resp = json.loads(resp.decode('utf-8'))
+                print(resp)
                 embed = discord.Embed(
                 title = "YUI Weather",
                 description = f"{resp['forecasts'][0]['dateLabel']}の天気は{resp['forecasts'][0]['telop']}\n最低気温は摂氏{resp['forecasts'][0]['temperature']['min']['celsius']}℃:華氏{resp['forecasts'][0]['temperature']['min']['fahrenheit']}°F\n最高気温は摂氏{resp['forecasts'][0]['temperature']['max']['celsius']}℃:華氏{resp['forecasts'][0]['temperature']['max']['fahrenheit']}°Fです\n{resp['forecasts'][1]['dateLabel']}の天気は{resp['forecasts'][0]['telop']}\n最低気温は摂氏{resp['forecasts'][1]['temperature']['min']['celsius']}℃:華氏{resp['forecasts'][1]['temperature']['min']['fahrenheit']}°F\n最高気温は摂氏{resp['forecasts'][1]['temperature']['max']['celsius']}℃:華氏{resp['forecasts'][1]['temperature']['max']['fahrenheit']}°Fです\n{resp['forecasts'][2]['dateLabel']}の天気は{resp['forecasts'][2]['telop']}\n最低気温は摂氏{resp['forecasts'][2]['temperature']['min']['celsius']}℃:華氏{resp['forecasts'][2]['temperature']['min']['fahrenheit']}°F\n最高気温は摂氏{resp['forecasts'][2]['temperature']['max']['celsius']}℃:華氏{resp['forecasts'][2]['temperature']['max']['fahrenheit']}°Fです",
