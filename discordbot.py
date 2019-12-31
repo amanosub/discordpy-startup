40d40a9a00c2e1696622c5370b97f42896c8012f
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-12-31 13:15:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1536,7 +1536,7 @@ async def on_message(message):
                
                 resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=%s'%cityc).read()
                 resp = json.loads(resp.decode('utf-8'))
-                7
+                print (resp)
                 desc = f"{(resp[forecasts][0][date]).split('-')[1]}月{(resp[forecasts][0][date]).split('-')[2]}日{resp[forecasts][0][dateLabel]}の天気は**{resp[forecasts][0][telop]}**"
                 desc += f"最高気温**{resp[forecasts][0][temperture][max]}℃/{resp[forecasts][0][fahrenheit][max]}℉**"
                 desc += f"最低気温**{resp[forecasts][0][temperture][min]}℃/{resp[forecasts][0][fahrenheit][min]}℉**"
