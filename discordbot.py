a68e37a5c4cb5a4889a06f76312de3ed3393c2ce
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-12-31 10:25:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1853,7 +1853,7 @@ description=f"**{message.author}**さんの\n```{message.content}```という発
                     title = "コマンドエラー",
                     description = f"```{{message.content}```というコマンドが**存在しない**か、**該当するシステムが破損している**、または**使い方が間違っている**可能性があります。\nhelpに乗っているにも関わらず動作しないコマンドを見つけた場合、お手数ですが```y!report [内容]```で、開発者までお知らせください。",
                     color = discord.Color.red())
-                embed.set_footer(icon_url = message.author.avatar_url,text=コマンド使用者｜{message.author.name})
+                embed.set_footer(icon_url = message.author.avatar_url,text="コマンド使用者｜{message.author.name}")
                 await message.channel.send(embed = embed)
 
 
