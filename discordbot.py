5bb6e53020ea9f5c518ca945a41c34932ca31fd8
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2020-01-04 01:28:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1930,7 +1930,7 @@ async def on_member_join(member):
 
 @client.event
 async def on_message_edit(*messages):
-    if "正解" in messages[1].embeds[0].description and messages[1].channel == t_ch and t_flag == True:
+    if "正解" in *messages[1].embeds[0].description and *messages[1].channel == t_ch and t_flag == True:
         await asyncio.sleep(0.2)
         await t_ch.send("::t Training")
 
