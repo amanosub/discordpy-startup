6b851c875142b5c72ad4adea1f3673544211a67f
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2020-01-05 13:27:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -96,8 +96,7 @@ async def on_ready():
 
     client = discord.Client()
     global d_ch      #◆世界樹の第一階層チャンネル取得
-    guild_num=654086105699844108
-    d_ch = discord.utils.get(client.get_guild(guild_num).text_channels, name=f'第{d_num}階層')
+    d_ch = discord.utils.get((client.get_guild(654086105699844108)).text_channels, name=f'第{d_num}階層')
 
     global d_ch2     #◆黒鉄城のチャンネル取得
     d_ch2= client.get_channel(654710356622704662)
