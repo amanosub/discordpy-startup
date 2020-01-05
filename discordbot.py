ded112ca7e61648c08f7435f72c80f82e1e07a38
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2020-01-05 12:48:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -300,9 +300,10 @@ async def on_message(message):
         global d_flag2
         mio = client.get_user(644153226597498890)
         tao = client.get_user(526620171658330112)
-        d_num01=d_ch2.name.split('第')[1]
-        d_num02=d_num01.split('層')[0]
-        d_num2=int(d_num02)
+        if not ch2 == int
+            d_num01=d_ch2.name.split('第')[1]
+            d_num02=d_num01.split('層')[0]
+            d_num2=int(d_num02)
 #━━━━❮Trainingコード❯━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━#
 
         me = client.user
