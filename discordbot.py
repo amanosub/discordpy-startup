b439990918b67c194473bb7dd1772069dc0b1a8c
amanosub <60776055+amanosub@users.noreply.github.com>
2020-02-07 23:22:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -58,7 +58,7 @@ async def on_ready():
     amano = client.get_user(446610711230152706)
     await amano.send(datetime.now(JST).strftime("%Y/%m/%d %H:%M:%S"))
     print(datetime.now(JST).strftime("%Y/%m/%d %H:%M:%S"))
-    loop.start()
+    #loop.start()
 
 @client.event
 async def on_message(message):
@@ -83,12 +83,12 @@ async def on_message(message):
         sent = f"\n**現在ノ討伐数**：`{m_num}`\n**停止検知回数**：`{stop_num}`\n**死亡復活回数：**`{revive_num}`\n**総ダメージ数：**`{all_damage}`\n**単発平均火力：**`{(round((all_damage)/(atk_num)))}`\n**総獲得経験値：**`{all_exp}`"
 
 
-    if message.content=='a)stop' and test_flag==True and message.author==me:
+    if message.content=='a))stop' and test_flag==True and message.author==me:
         test_flag=False
         test_ch=None
         await message.channel.send(f'**__Auto Battle System Stop__**\n**戦闘開始時刻**：{start_time}\n**総合敵討伐数**：{m_num}\n**停止検知回数**：{stop_num}\n**死亡復活回数**：{revive_num}')
         
-    if message.content.startswith("a)start") and message.author==me:
+    if message.content.startswith("a))start") and message.author==me:
         test_flag = True
         test_ch = message.channel
         start_time = datetime.now(JST).strftime("%Y/%m/%d %H:%M:%S")
@@ -186,6 +186,8 @@ async def on_message(message):
             await asyncio.sleep(0.2)
             await test_ch.send(f'::attack {sent}')
 
+        if '不正解です' in message.content:
+            test_flag=False
 
         if message.embeds[0].title and '戦闘結果' in message.embeds[0].title:
             fb_flag = False
